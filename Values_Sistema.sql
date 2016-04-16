3b5da7d80299e00daf04a5639d442e52e445408f
Jpfonseca <jpedrofonseca_94@hotmail.com>
2016-04-16 02:09:03 +0100

    Added files via upload

---
+++ Values_Sistema.sql
@@ -0,0 +1,19 @@
+USE Sistema;
+
+--Pessoa Values
+
+INSERT Pessoa VALUES ('1234, Antonio Alves');
+INSERT Pessoa VALUES ('1235, Alvaro Antunes');
+INSERT Pessoa VALUES ('12324, Barbara Alves');
+INSERT Pessoa VALUES ('12357, Bernardo Costa');
+INSERT Pessoa VALUES (', ');
+INSERT Pessoa VALUES (', ');
+INSERT Pessoa VALUES (', ');
+INSERT Pessoa VALUES (', ');
+INSERT Pessoa VALUES (', ');
+INSERT Pessoa VALUES (', ');
+INSERT Pessoa VALUES (', ');
+INSERT Pessoa VALUES (', ');
+INSERT Pessoa VALUES (', ');
+INSERT Pessoa VALUES (', ');
+ 
