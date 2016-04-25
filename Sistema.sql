c1e3034c4c9dcab55025a160aef3867c1bb556b2
Jpfonseca <jpedrofonseca_94@hotmail.com>
2016-04-25 21:29:59 +0100

    Added files via upload
    Correção de Erros

--- Sistema.sql
+++ Sistema.sql
@@ -5,7 +5,7 @@ use Sistema;
 
 CREATE TABLE Pessoa (
 	CC INT NOT NULL,
-	Nome varCHAR(128) CHECK (Nome like '([a-z]|[A-Z])+'),
+	Nome varCHAR(128) CHECK (Nome like '[a-Z][a-Z]%'),
 	Constraint Pk_CC PRIMARY KEY (CC)
 );
 
@@ -53,8 +53,8 @@ CREATE TABLE Empresa (
 
 CREATE TABLE Itinerario_da_Viagem (
 	ID_v smallint NOT NULL IDENTITY(1,1),
-	Local_Partida varchar(64) NOT NULL CHECK (Local_Partida like '([a-z]|[A-Z])+'),
-	Local_Destino varchar(64) NOT NULL CHECK (Local_Destino like '([a-z]|[A-Z])+'),
+	Local_Partida varchar(64) NOT NULL CHECK (Local_Partida like '[a-Z][a-Z]%'),
+	Local_Destino varchar(64) NOT NULL CHECK (Local_Destino like '[a-Z][a-Z]%'),
 	Hora_partida_origem DATE,
 	N_etapas smallint CHECK (N_etapas<1000),
 	Constraint Pk_ID_v Primary key(ID_v)
@@ -63,14 +63,14 @@ CREATE TABLE Itinerario_da_Viagem (
 CREATE TABLE Transporte (
 	ID_t smallint NOT NULL IDENTITY(1,1),
 	Bilhete smallint NOT NULL,
-	Companhia varchar(64) CHECK  (Companhia like '([a-z]|[A-Z])+'),
+	Companhia varchar(64) CHECK  (Companhia like '[a-Z][a-Z]%'),
 	Constraint Pk_ID PRIMARY KEY (ID_t)
 );
 
 CREATE TABLE Tipo_Transporte (
 	ID_Tipo_Transporte smallint NOT NULL,
 	ID_transporte_fk smallint NOT NULL,
-	Designacao varchar(64) CHECK  (Designacao like '([a-z]|[A-Z])+') ,
+	Designacao varchar(64) CHECK  (Designacao like '[a-Z][a-Z]%') ,
 	Constraint Pk_ID_Tipo_Transporte PRIMARY KEY (ID_Tipo_Transporte),
 	CONSTRAINT Fk_ID_transporte FOREIGN KEY (ID_transporte_fk) REFERENCES Transporte(ID_t)
 );
@@ -91,7 +91,7 @@ CREATE TABLE Etapas (
 CREATE TABLE Estadia (
 	ID_Estadia smallint NOT NULL,
 	Morada varchar(128),
-	Pais varchar(32) CHECK (Pais like '([a-z]|[A-Z])+'),
+	Pais varchar(32) CHECK (Pais like '[a-Z][a-Z]%'),
 	Custo float,
 	Duracao smallint,
 	N_Reserva_fk int NOT NULL,
