b921c57e6a55ae8df6131feb91aeecc7b357a552
Jpfonseca <jpedrofonseca_94@hotmail.com>
2016-06-06 16:35:12 +0100

    Security update

--- 8_Security.sql
+++ 8_Security.sql
@@ -5,17 +5,39 @@ GO
 EXECUTE sp_droplogin 'admins';
 GO
 EXECUTE sp_addlogin 'admins', 'basededados','Sistema','English'
+GO
+DROP USER random_owner;
+GO
+CREATE USER random_owner FOR LOGIN admins;
+GO
 
-DROP USER random_guest;
+EXECUTE sp_droplogin 'func';
 GO
-CREATE USER random_guest FOR LOGIN admins;
+EXECUTE sp_addlogin 'func', 'basededados2','Sistema','English'
 GO
+DROP USER random_func;
+GO
+CREATE USER random_func FOR LOGIN func;
+GO
+
 
 EXECUTE sp_droprole 'MyRole2';
 GO
 EXECUTE sp_addrole 'MyRole2'
 GO
-GRANT EXECUTE, SELECT TO MyRole2;
-GO
+GRANT EXECUTE,SELECT,INSERT TO MyRole2;
 
-EXEC sp_addrolemember 'MyRole2', 'random_guest';
+
+GO
+EXECUTE sp_droprolemember'MyRole2','random_func'
+GO
+EXECUTE sp_addrolemember 'MyRole2', 'random_func'
+GO 
+
+EXECUTE sp_droprolemember'db_securityadmin','random_owner'
+
+GO
+EXECUTE sp_addrolemember 'db_securityadmin','random_owner'
+
+
+
